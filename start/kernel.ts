055d14d797b45afde711ce89069c8c7905ea5a50
Lucas Maranho <lucas.maranho@ostenmoove.com.br>
2024-11-17 16:04:34 -0300

    ajustes projeto

--- start/kernel.ts
+++ start/kernel.ts
@@ -8,7 +8,6 @@
 |
 */
 
-import router from '@adonisjs/core/services/router'
 import server from '@adonisjs/core/services/server'
 
 /**
@@ -32,12 +31,8 @@ server.use([
  * The router middleware stack runs middleware on all the HTTP
  * requests with a registered route.
  */
-router.use([() => import('@adonisjs/core/bodyparser_middleware'), () => import('@adonisjs/auth/initialize_auth_middleware')])
 
 /**
  * Named middleware collection must be explicitly assigned to
  * the routes or the routes group.
  */
-export const middleware = router.named({
-  auth: () => import('#middleware/auth_middleware')
-})
